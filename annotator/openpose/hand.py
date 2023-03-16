76810f1079da4c5e6fef3b290fabbce1598673a1
lvmin <lyuminzhang@outlook.com>
2023-03-15 19:16:18 -0700

    i

--- annotator/openpose/hand.py
+++ annotator/openpose/hand.py
@@ -36,7 +36,7 @@ class Hand(object):
 
         Hr, Wr, Cr = oriImgRaw.shape
 
-        oriImg = cv2.GaussianBlur(oriImgRaw, (0, 0), 0.5)
+        oriImg = cv2.GaussianBlur(oriImgRaw, (0, 0), 0.8)
 
         for m in range(len(multiplier)):
             scale = multiplier[m]
