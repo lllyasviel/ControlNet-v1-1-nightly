b4e8aec80372214d1dda158785420054b9311ad8
lvmin <lyuminzhang@outlook.com>
2023-04-12 11:49:25 -0700

    i

--- gradio_lineart_anime.py
+++ gradio_lineart_anime.py
@@ -93,6 +93,9 @@ with block:
         with gr.Column():
             input_image = gr.Image(source='upload', type="numpy")
             prompt = gr.Textbox(label="Prompt")
+            a_prompt = gr.Textbox(label="Added Prompt (Usually do not need to change)", value='masterpiece, best quality, ultra-detailed, illustration, disheveled hair')
+            n_prompt = gr.Textbox(label="Negative Prompt (Usually do not need to change)",
+                                  value='longbody, lowres, bad anatomy, bad hands, missing fingers, pubic hair,extra digit, fewer digits, cropped, worst quality, low quality')
             run_button = gr.Button(label="Run")
             num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
             seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=12345)
@@ -104,8 +107,6 @@ with block:
                 ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
                 scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
                 eta = gr.Slider(label="DDIM ETA", minimum=0.0, maximum=1.0, value=1.0, step=0.01)
-                a_prompt = gr.Textbox(label="Added Prompt", value='masterpiece, best quality, ultra-detailed, illustration, disheveled hair')
-                n_prompt = gr.Textbox(label="Negative Prompt", value='longbody, lowres, bad anatomy, bad hands, missing fingers, pubic hair,extra digit, fewer digits, cropped, worst quality, low quality')
         with gr.Column():
             result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
     ips = [det, input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, detect_resolution, ddim_steps, strength, scale, seed, eta]
