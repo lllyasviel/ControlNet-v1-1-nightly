505f5eaec81001932272d7489c160c7047ee385d
lvmin <lyuminzhang@outlook.com>
2023-04-12 15:21:23 -0700

    i

--- gradio_claion_normalbae.py
+++ gradio_claion_normalbae.py
@@ -17,7 +17,7 @@ from cldm.ddim_hacked import DDIMSampler
 
 preprocessor = None
 
-model_name = 'control_v11p_sd15_normal'
+model_name = 'control_v11p_sd15_normalbae'
 model = create_model(f'./models/{model_name}.yaml').cpu()
 model.load_state_dict(load_state_dict('./models/v1-5-pruned.ckpt', location='cuda'), strict=False)
 model.load_state_dict(load_state_dict(f'./models/{model_name}.pth', location='cuda'), strict=False)
